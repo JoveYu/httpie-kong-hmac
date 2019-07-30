8736a3c2b0b07020b66cda3e846add9480a9d0d9
Jove Yu <yushijun110@126.com>
2019-07-30 16:21:28 +0800

    fix body encode

--- httpie_kong_hmac.py
+++ httpie_kong_hmac.py
@@ -3,10 +3,11 @@ import datetime
 import hashlib
 import base64
 import hmac
+import traceback
 
 from httpie.plugins import AuthPlugin
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __author__ = 'Jove Yu'
 __license__ = 'MIT'
 
@@ -22,23 +23,26 @@ class KongHMAC:
         self.auth_template = 'hmac username="{}", algorithm="{}", headers="{}", signature="{}"'
 
     def __call__(self, r):
+        try:
 
-        # add Date header
-        if 'date' in self.headers and 'Date' not in r.headers:
-            r.headers['Date'] = self.create_date_header()
+            # add Date header
+            if 'date' in self.headers and 'Date' not in r.headers:
+                r.headers['Date'] = self.create_date_header()
 
-        # add Digest header
-        if r.body:
-            r.headers['Digest'] = 'SHA-256={}'.format(self.get_body_digest(r))
-            if 'digest' not in self.headers:
-                self.headers.append('digest')
+            # add Digest header
+            if r.body:
+                r.headers['Digest'] = 'SHA-256={}'.format(self.get_body_digest(r))
+                if 'digest' not in self.headers:
+                    self.headers.append('digest')
 
-        # get sign
-        sign = self.get_sign(r)
-        r.headers['Authorization'] = self.auth_template.format(self.username,
-                            self.algorithm, ' '.join(self.headers), sign)
+            # get sign
+            sign = self.get_sign(r)
+            r.headers['Authorization'] = self.auth_template.format(self.username,
+                                self.algorithm, ' '.join(self.headers), sign)
 
-        return r
+            return r
+        except:
+            traceback.print_exc()
 
     def create_date_header(self):
         now = datetime.datetime.utcnow()
@@ -59,7 +63,7 @@ class KongHMAC:
 
     def get_body_digest(self, r):
         if r.body:
-            h = hashlib.sha256(r.body)
+            h = hashlib.sha256(r.body.encode(self.charset))
             return base64.b64encode(h.digest()).decode(self.charset)
         return ''
 
